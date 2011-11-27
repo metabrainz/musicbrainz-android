04968a7e809d30d1bd00eb39d00cce0aed3ebd1c
Jamie McDonald <jdamcd@gmail.com>
2011-11-27 18:03:13 +0000

    Asset populated text view is now a widget.

--- app/src/org/musicbrainz/mobile/activity/AboutActivity.java
+++ app/src/org/musicbrainz/mobile/activity/AboutActivity.java
@@ -22,10 +22,9 @@ package org.musicbrainz.mobile.activity;
 
 import org.musicbrainz.mobile.R;
 import org.musicbrainz.mobile.activity.base.MusicBrainzActivity;
-import org.musicbrainz.mobile.util.Utils;
+import org.musicbrainz.mobile.widget.AssetTextView;
 
 import android.os.Bundle;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.TextView;
@@ -41,8 +40,8 @@ public class AboutActivity extends MusicBrainzActivity {
         TextView versionLabel = (TextView) findViewById(R.id.version_text);
         versionLabel.setText(getText(R.string.version_text) + " " + getVersion());
         
-        TextView body = (TextView) findViewById(R.id.about_text);
-        body.setText(Html.fromHtml(Utils.stringFromAsset(this, "about.html")));
+        AssetTextView body = (AssetTextView) findViewById(R.id.about_text);
+        body.setAsset("about.html");
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/org/musicbrainz/mobile/widget/AssetTextView.java
@@ -0,0 +1,23 @@
+package org.musicbrainz.mobile.widget;
+
+import org.musicbrainz.mobile.util.Utils;
+
+import android.content.Context;
+import android.text.Html;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class AssetTextView extends TextView {
+    
+    private Context context;
+    
+    public AssetTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+    
+    public void setAsset(String asset) {
+        setText(Html.fromHtml(Utils.stringFromAsset(context, asset)));
+    }
+
+}
