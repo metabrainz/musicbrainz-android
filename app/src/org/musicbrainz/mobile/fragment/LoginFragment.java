858b7ed52afcd5069569c7b8c40db781df3d8a2e
Jamie McDonald <jdamcd@gmail.com>
2012-03-19 17:59:52 +0000

    Fixed build fail.

--- app/src/org/musicbrainz/mobile/fragment/LoginFragment.java
+++ app/src/org/musicbrainz/mobile/fragment/LoginFragment.java
@@ -13,6 +13,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_login, container);
+        return layout;
     }
 
 }
