6a853e49f975bf5c1e16ab39649502fcba503f1a
Jamie McDonald <jdamcd@gmail.com>
2012-03-31 23:20:57 +0100

    Updated some tests.

--- robotium/src/org/musicbrainz/mobile/test/robotium/navigation/DashboardNavigationTest.java
+++ robotium/src/org/musicbrainz/mobile/test/robotium/navigation/DashboardNavigationTest.java
@@ -23,8 +23,11 @@ package org.musicbrainz.mobile.test.robotium.navigation;
 import org.musicbrainz.mobile.activity.AboutActivity;
 import org.musicbrainz.mobile.activity.DashboardActivity;
 import org.musicbrainz.mobile.activity.DonateActivity;
+import org.musicbrainz.mobile.activity.LoginActivity;
+import org.musicbrainz.mobile.activity.SettingsActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
 
 import com.jayway.android.robotium.solo.Solo;
 
@@ -41,36 +44,39 @@ public class DashboardNavigationTest extends ActivityInstrumentationTestCase2<Da
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
-    public void testDashboardNavigation() throws Exception {
-        openCloseAbout();
-        openCloseDonate();
-        openCloseCollection();
-        openCloseSettings();
-        openCloseLogIn();
-    }
-
-    private void openCloseAbout() {
+    @Smoke
+    public void testOpenCloseAbout() {
         solo.clickOnButton("About");
         solo.assertCurrentActivity("Expected About Activity", AboutActivity.class);
         solo.goBack();
     }
 
-    private void openCloseDonate() {
+    @Smoke
+    public void testOpenCloseDonate() {
         solo.clickOnButton("Donate");
         solo.assertCurrentActivity("Expected Donate Activity", DonateActivity.class);
         solo.goBack();
     }
 
-    private void openCloseCollection() {
-        solo.clickOnButton("Collection");
+    @Smoke
+    public void testOpenCloseCollection() {
+        solo.clickOnButton("Collections");
+        solo.assertCurrentActivity("Expected Login Activity", LoginActivity.class);
+        solo.goBack();
     }
     
-    private void openCloseSettings() {
-        // TODO
+    @Smoke
+    public void testOpenCloseSettings() {
+        solo.clickOnMenuItem("Settings");
+        solo.assertCurrentActivity("Expected Settings Activity", SettingsActivity.class);
+        solo.goBack();
     }
     
-    private void openCloseLogIn() {
-        // TODO
+    @Smoke
+    public void testOpenCloseLogIn() {
+        solo.clickOnMenuItem("Log In");
+        solo.assertCurrentActivity("Expected Login Activity", LoginActivity.class);
+        solo.goBack();
     }
 
     @Override
