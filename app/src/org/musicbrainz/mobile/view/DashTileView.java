a443e3542665f035ed0e17f308fb22f9ed81ae72
Jamie McDonald <jdamcd@gmail.com>
2012-04-03 22:38:06 +0100

    Improved dash tile focusability.

--- app/src/org/musicbrainz/mobile/view/DashTileView.java
+++ app/src/org/musicbrainz/mobile/view/DashTileView.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2012 Jamie McDonald
+ * 
+ * This file is part of MusicBrainz for Android.
+ * 
+ * MusicBrainz for Android is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * MusicBrainz for Android is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MusicBrainz for Android. If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package org.musicbrainz.mobile.view;
 
 import org.musicbrainz.mobile.R;
@@ -26,6 +46,7 @@ public class DashTileView extends RelativeLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         setBackgroundResource(R.drawable.dash_grid);
+        setFocusable(true);
         setOnTouchListener(touchListener);
         icon = (ImageView) findViewById(R.id.dash_tile_icon);
         text = (TextView) findViewById(R.id.dash_tile_text);
