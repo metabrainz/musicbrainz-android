28a72878e978c7e29689e351ab8cc37360c70dc6
Jamie McDonald <jdamcd@gmail.com>
2012-09-23 22:59:24 +0100

    Adding example API key for bio data so branch compiles.

--- app/src/org/musicbrainz/mobile/config/Secrets.java
+++ app/src/org/musicbrainz/mobile/config/Secrets.java
@@ -9,6 +9,7 @@ public class Secrets {
 
     public static final String BUGSENSE_API_KEY = "99f3740e";
     public static final String PAYPAL_APP_ID = "APP-80W284485P519543T";
+    public static final String LASTFM_API_KEY = "b25b959554ed76058ac220b7b2e0a026";
 
     public String getKey() {
         return "aplaceholderstring";
