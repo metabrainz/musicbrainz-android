679f010ff9be8dc9a136636d9c1558c236a198af
Kartik Ohri <kartikohri13@gmail.com>
2019-06-13 14:34:38 +0530

    Fix recording collection bug.

--- app/src/main/java/org/metabrainz/mobile/presentation/features/collection/CollectionAdapterRecording.java
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/collection/CollectionAdapterRecording.java
@@ -13,8 +13,6 @@ import org.metabrainz.mobile.R;
 import org.metabrainz.mobile.data.sources.api.entities.mbentity.Recording;
 import org.metabrainz.mobile.intent.IntentFactory;
 import org.metabrainz.mobile.presentation.features.recording.RecordingActivity;
-import org.metabrainz.mobile.presentation.features.collection.EntityViewHolder;
-import org.metabrainz.mobile.presentation.features.collection.CollectionAdapter;
 import org.metabrainz.mobile.util.Log;
 
 import java.util.List;
@@ -46,7 +44,8 @@ public class CollectionAdapterRecording extends CollectionAdapter {
         Recording recording = data.get(position);
         viewHolder.recordingName.setText(recording.getTitle());
 
-        setViewVisibility(recording.getReleases().get(0).getTitle(), viewHolder.recordingRelease);
+        if (recording.getReleases().size() != 0)
+            setViewVisibility(recording.getReleases().get(0).getTitle(), viewHolder.recordingRelease);
         Log.d(recording.getDisplayArtist());
         setViewVisibility(recording.getDisplayArtist(), viewHolder.recordingArtist);
         setViewVisibility(recording.getDisambiguation(), viewHolder.recordingDisambiguation);
