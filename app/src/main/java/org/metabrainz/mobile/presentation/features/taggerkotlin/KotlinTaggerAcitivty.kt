666e9f2427e3d1e74618885712a253557bc2a659
SomalRudra <somalchakrabortyy@gmail.com>
2020-07-14 00:03:50 +0530

    test_rebase

---
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/taggerkotlin/KotlinTaggerAcitivty.kt
@@ -0,0 +1,12 @@
+package org.metabrainz.mobile.presentation.features.taggerkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.metabrainz.mobile.R
+
+class KotlinTaggerAcitivty: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kotlin_tagger_activity)
+    }
+}
