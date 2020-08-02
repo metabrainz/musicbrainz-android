9111d1e4ba3a39c4b951530990671ed04c1182b0
Kartik Ohri <kartikohri13@gmail.com>
2020-08-02 13:32:16 +0530

    Improve SplashActivity code
    Use coroutine instead of handler to launch activity. Rename package to use consistent case. Delete redundant code.

---
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/splash_screen/SplashActivity.kt
@@ -0,0 +1,43 @@
+package org.metabrainz.mobile.presentation.features.splash_screen
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.metabrainz.mobile.R
+import org.metabrainz.mobile.presentation.features.KotlinDashboard.KotlinDashboardActivity
+import org.metabrainz.mobile.presentation.features.OnBoarding.AllowMe
+import org.metabrainz.mobile.presentation.features.login.LoginSharedPreferences
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        val splashImage: ImageView = findViewById(R.id.splash_image)
+        splashImage.animation = AnimationUtils.loadAnimation(this,
+                R.anim.splashscreen_middle_animation)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            if (LoginSharedPreferences.getSkipState())
+                startActivity(Intent(this@SplashActivity, KotlinDashboardActivity::class.java))
+            else
+                startActivity(Intent(this@SplashActivity, AllowMe::class.java))
+            finish()
+        }
+    }
+}
