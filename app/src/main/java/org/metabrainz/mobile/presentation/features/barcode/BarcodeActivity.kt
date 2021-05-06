66a154021c75a4deead5cfbec391c1f769b38488
akshaaatt <tiwariakshat03@gmail.com>
2021-05-06 14:59:49 +0530

    - Added the Google ML-Kit Barcode Scanner Implementation.

--- app/src/main/java/org/metabrainz/mobile/presentation/features/barcode/BarcodeActivity.kt
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/barcode/BarcodeActivity.kt
@@ -1,40 +1,196 @@
 package org.metabrainz.mobile.presentation.features.barcode
 
 import android.Manifest
-import android.app.Activity
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.os.Bundle
-import android.view.ViewGroup
+import android.view.View
+import android.view.View.*
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import me.dm7.barcodescanner.zbar.Result
-import me.dm7.barcodescanner.zbar.ZBarScannerView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.common.internal.Objects
+import com.google.android.material.chip.Chip
+import com.limerse.mlkit.barcodedetection.BarcodeProcessor
+import com.limerse.mlkit.camera.CameraSource
+import com.limerse.mlkit.camera.CameraSourcePreview
+import com.limerse.mlkit.camera.GraphicOverlay
+import com.limerse.mlkit.camera.WorkflowModel
 import org.metabrainz.mobile.R
+import java.io.IOException
+import java.util.*
 
-class BarcodeActivity : Activity(), ZBarScannerView.ResultHandler {
-    private var scannerView: ZBarScannerView? = null
+class BarcodeActivity : AppCompatActivity(), OnClickListener {
+
+    private var cameraSource: CameraSource? = null
+    private var preview: CameraSourcePreview? = null
+    private var graphicOverlay: GraphicOverlay? = null
+    private var flashButton: View? = null
+    private var promptChip: Chip? = null
+    private var promptChipAnimator: AnimatorSet? = null
+    private var workflowModel: WorkflowModel? = null
+    private var currentWorkflowState: WorkflowModel.WorkflowState? = null
+
     private var cameraPermission = false
     private val CAMERA_PERMISSION = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_barcode)
-        val contentFrame = findViewById<ViewGroup>(R.id.content_frame)
-        scannerView = ZBarScannerView(this)
-        contentFrame.addView(scannerView)
-        scannerView!!.setResultHandler(this)
         cameraPermission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
         if (!cameraPermission) ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION)
+
+        setContentView(R.layout.activity_barcode)
+        preview = findViewById(R.id.camera_preview)
+        graphicOverlay = findViewById<GraphicOverlay>(R.id.camera_preview_graphic_overlay).apply {
+            setOnClickListener(this@BarcodeActivity)
+            cameraSource = CameraSource(this)
+        }
+
+        promptChip = findViewById(R.id.bottom_prompt_chip)
+        promptChipAnimator =
+            (AnimatorInflater.loadAnimator(this,
+                R.animator.bottom_prompt_chip_enter
+            ) as AnimatorSet).apply {
+                setTarget(promptChip)
+            }
+
+        findViewById<View>(R.id.close_button).setOnClickListener(this)
+        flashButton = findViewById<View>(R.id.flash_button).apply {
+            setOnClickListener(this@BarcodeActivity)
+        }
+
+        setUpWorkflowModel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        workflowModel?.markCameraFrozen()
+        currentWorkflowState = WorkflowModel.WorkflowState.NOT_STARTED
+        cameraSource?.setFrameProcessor(BarcodeProcessor(graphicOverlay!!, workflowModel!!))
+        workflowModel?.setWorkflowState(WorkflowModel.WorkflowState.DETECTING)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        currentWorkflowState = WorkflowModel.WorkflowState.NOT_STARTED
+        stopCameraPreview()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraSource?.release()
+        cameraSource = null
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.close_button -> onBackPressed()
+            R.id.flash_button -> {
+                flashButton?.let {
+                    if (it.isSelected) {
+                        it.isSelected = false
+                        cameraSource?.updateFlashMode(Camera.Parameters.FLASH_MODE_OFF)
+                    } else {
+                        it.isSelected = true
+                        cameraSource!!.updateFlashMode(Camera.Parameters.FLASH_MODE_TORCH)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun startCameraPreview() {
+        val workflowModel = this.workflowModel ?: return
+        val cameraSource = this.cameraSource ?: return
+        if (!workflowModel.isCameraLive) {
+            try {
+                workflowModel.markCameraLive()
+                preview?.start(cameraSource)
+            } catch (e: IOException) {
+                cameraSource.release()
+                this.cameraSource = null
+            }
+        }
+    }
+
+    private fun stopCameraPreview() {
+        val workflowModel = this.workflowModel ?: return
+        if (workflowModel.isCameraLive) {
+            workflowModel.markCameraFrozen()
+            flashButton?.isSelected = false
+            preview?.stop()
+        }
+    }
+
+    private fun setUpWorkflowModel() {
+        workflowModel = ViewModelProvider(this).get(WorkflowModel::class.java)
+
+        // Observes the workflow state changes, if happens, update the overlay view indicators and
+        // camera preview state.
+        workflowModel!!.workflowState.observe(this, Observer { workflowState ->
+            if (workflowState == null || Objects.equal(currentWorkflowState, workflowState)) {
+                return@Observer
+            }
+
+            currentWorkflowState = workflowState
+
+            val wasPromptChipGone = promptChip?.visibility == GONE
+
+            when (workflowState) {
+                WorkflowModel.WorkflowState.DETECTING -> {
+                    promptChip?.visibility = VISIBLE
+                    promptChip?.setText(R.string.prompt_point_at_a_barcode)
+                    startCameraPreview()
+                }
+                WorkflowModel.WorkflowState.CONFIRMING -> {
+                    promptChip?.visibility = VISIBLE
+                    promptChip?.setText(R.string.prompt_move_camera_closer)
+                    startCameraPreview()
+                }
+                WorkflowModel.WorkflowState.SEARCHING -> {
+                    promptChip?.visibility = VISIBLE
+                    promptChip?.setText(R.string.prompt_searching)
+                    stopCameraPreview()
+                }
+                WorkflowModel.WorkflowState.DETECTED, WorkflowModel.WorkflowState.SEARCHED -> {
+                    promptChip?.visibility = GONE
+                    stopCameraPreview()
+                }
+                else -> promptChip?.visibility = GONE
+            }
+
+            val shouldPlayPromptChipEnteringAnimation = wasPromptChipGone && promptChip?.visibility == VISIBLE
+            promptChipAnimator?.let {
+                if (shouldPlayPromptChipEnteringAnimation && !it.isRunning) it.start()
+            }
+        })
+
+        workflowModel?.detectedBarcode?.observe(this) { barcode ->
+            if (barcode != null) {
+                Toast.makeText(this, "Contents = " + barcode.rawValue +
+                        ", Format = " + barcode.format, Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, BarcodeResultActivity::class.java)
+                intent.putExtra("barcode", barcode.rawValue)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        cameraPermission = requestCode == CAMERA_PERMISSION && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
-        if (cameraPermission) scannerView!!.startCamera() else {
+        cameraPermission = requestCode == CAMERA_PERMISSION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        if (!cameraPermission) {
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Camera Permission Denied")
             builder.setMessage("You need to grant MusicBrainz for Android access to your device's " +
@@ -44,24 +200,8 @@ class BarcodeActivity : Activity(), ZBarScannerView.ResultHandler {
             builder.setOnDismissListener { dialog: DialogInterface? -> finish() }
             builder.create().show()
         }
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        if (cameraPermission) scannerView!!.stopCamera()
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        if (cameraPermission) scannerView!!.startCamera()
-    }
-
-    override fun handleResult(rawResult: Result) {
-        Toast.makeText(this, "Contents = " + rawResult.contents +
-                ", Format = " + rawResult.barcodeFormat.name, Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, BarcodeResultActivity::class.java)
-        intent.putExtra("barcode", rawResult.contents)
-        startActivity(intent)
-        finish()
+        else {
+            recreate()
+        }
     }
 }
