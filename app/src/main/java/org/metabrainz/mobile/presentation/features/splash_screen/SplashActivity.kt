d2edce4b3ce609de4fc05832923d5843f503d801
akshaaatt <tiwariakshat03@gmail.com>
2021-08-16 17:08:28 +0530

    Splash Screen Update
    - This inherently displays the splash screen without any breaks or external work needed.

--- app/src/main/java/org/metabrainz/mobile/presentation/features/splash_screen/SplashActivity.kt
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/splash_screen/SplashActivity.kt
@@ -2,14 +2,11 @@ package org.metabrainz.mobile.presentation.features.splash_screen
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.metabrainz.mobile.R
 import org.metabrainz.mobile.presentation.UserPreferences
 import org.metabrainz.mobile.presentation.features.dashboard.DashboardActivity
 import org.metabrainz.mobile.presentation.features.onboarding.FeaturesActivity
@@ -17,10 +14,6 @@ import org.metabrainz.mobile.presentation.features.onboarding.FeaturesActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        val splashImage: ImageView = findViewById(R.id.splash_image)
-        splashImage.animation = AnimationUtils.loadAnimation(this, R.anim.splashscreen_middle_animation)
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(2000)
