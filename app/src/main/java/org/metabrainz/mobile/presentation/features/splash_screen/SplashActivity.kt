59b335a043e0af77cbff02ae71c5b94a2b78f322
akshaaatt <tiwariakshat03@gmail.com>
2021-08-16 20:07:03 +0530

    Subtle fix

--- app/src/main/java/org/metabrainz/mobile/presentation/features/splash_screen/SplashActivity.kt
+++ app/src/main/java/org/metabrainz/mobile/presentation/features/splash_screen/SplashActivity.kt
@@ -3,10 +3,6 @@ package org.metabrainz.mobile.presentation.features.splash_screen
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.metabrainz.mobile.presentation.UserPreferences
 import org.metabrainz.mobile.presentation.features.dashboard.DashboardActivity
 import org.metabrainz.mobile.presentation.features.onboarding.FeaturesActivity
@@ -15,15 +11,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
-            if (UserPreferences.onBoardingStatus) {
-                startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
-            }
-            else {
-                startActivity(Intent(this@SplashActivity, FeaturesActivity::class.java))
-            }
-            finish()
+        if (UserPreferences.onBoardingStatus) {
+            startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
         }
+        else {
+            startActivity(Intent(this@SplashActivity, FeaturesActivity::class.java))
+        }
+        finish()
     }
 }
