a5207e48d843c0c9a321e3ade71594dbf2eb3958
akshaaatt <tiwariakshat03@gmail.com>
2022-04-23 15:57:49 +0530

    Networking calls made

---
+++ app/src/main/java/org/metabrainz/android/data/repository/ListensRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package org.metabrainz.android.data.repository
+
+import androidx.annotation.WorkerThread
+import org.metabrainz.android.data.sources.CollectionUtils
+import org.metabrainz.android.data.sources.api.CollectionService
+import org.metabrainz.android.data.sources.api.ListensService
+import org.metabrainz.android.data.sources.api.entities.CoverArt
+import org.metabrainz.android.data.sources.api.entities.listens.Listen
+import org.metabrainz.android.data.sources.api.entities.listens.Listens
+import org.metabrainz.android.data.sources.api.entities.mbentity.Collection
+import org.metabrainz.android.util.Resource
+import org.metabrainz.android.util.Resource.Status.FAILED
+import org.metabrainz.android.util.Resource.Status.SUCCESS
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ListensRepositoryImpl @Inject constructor(val service: ListensService) : ListensRepository{
+
+    @WorkerThread
+    override suspend fun fetchUserListens(userName: String): Resource<List<Listen>> {
+        return try {
+            val response = service.getUserListens(user_name = userName)
+            Resource(SUCCESS, response.payload.listens)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource(FAILED, null)
+        }
+    }
+
+    override suspend fun fetchCoverArt(MBID: String?): Resource<CoverArt> {
+        return try {
+            val coverArt = service.getCoverArt(MBID)
+            Resource(SUCCESS, coverArt)
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            Resource.failure()
+        }
+    }
+}
