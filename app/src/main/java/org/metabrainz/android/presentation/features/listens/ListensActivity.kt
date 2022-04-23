d6b90a578b44d5d7ab82d6d4838fe5cdbf613042
akshaaatt <tiwariakshat03@gmail.com>
2022-04-23 20:05:27 +0530

    Make generic calls

--- app/src/main/java/org/metabrainz/android/presentation/features/listens/ListensActivity.kt
+++ app/src/main/java/org/metabrainz/android/presentation/features/listens/ListensActivity.kt
@@ -3,18 +3,21 @@ package org.metabrainz.android.presentation.features.listens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
 import org.metabrainz.android.R
 import org.metabrainz.android.presentation.components.BottomNavigationBar
 import org.metabrainz.android.presentation.components.ListenCard
 import org.metabrainz.android.presentation.components.TopAppBar
+import org.metabrainz.android.presentation.features.login.LoginSharedPreferences.username
 
 @AndroidEntryPoint
 class ListensActivity: ComponentActivity() {
@@ -27,7 +30,9 @@ class ListensActivity: ComponentActivity() {
                 topBar = { TopAppBar(activity = this, title = "Listens") },
                 bottomBar = { BottomNavigationBar(activity = this) }
             ) {
-                AllUserListens()
+                AllUserListens(
+                    modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                )
             }
         }
     }
@@ -38,12 +43,15 @@ fun AllUserListens(
     viewModel: ListensViewModel = viewModel(),
     modifier: Modifier = Modifier
 ) {
-    viewModel.fetchUserListens("akshaaatt")
+    username?.let { viewModel.fetchUserListens(userName = it) }
     LazyColumn(modifier) {
-            items(viewModel.listens) {listen->
+            items(viewModel.listens) { listen->
                 when {
                     listen.track_metadata.additional_info.release_mbid!=null -> {
                         viewModel.fetchCoverArt(listen.track_metadata.additional_info.release_mbid)
+                    }
+                    listen.track_metadata.mbid_mapping.release_mbid!=null -> {
+                        viewModel.fetchCoverArt(listen.track_metadata.mbid_mapping.release_mbid)
                     }
                 }
                 ListenCard(
