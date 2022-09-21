3b8d73cabb979eb89c26626ebf3158815c973ff8
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-09-21 10:07:43 +0530

    ticket mobile-79 fix: granual permissions for api 33

--- app/src/main/java/org/metabrainz/android/presentation/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/org/metabrainz/android/presentation/features/dashboard/DashboardActivity.kt
@@ -1,12 +1,14 @@
 package org.metabrainz.android.presentation.features.dashboard
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
@@ -23,23 +25,28 @@ import org.metabrainz.android.presentation.features.onboarding.FeaturesActivity
 @AndroidEntryPoint
 class DashboardActivity : ComponentActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     @OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-
         if (!PreferenceManager.getDefaultSharedPreferences(this).getBoolean("onboarding", false)) {
             startActivity(Intent(this, FeaturesActivity::class.java))
             finish()
         }
+        val neededPermissions = arrayOf(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_MEDIA_AUDIO
+        )
         setContent {
             var isGrantedPerms by remember {
                 mutableStateOf(false)
             }
             val launcher = rememberLauncherForActivityResult(
                 contract =
-                ActivityResultContracts.RequestPermission()
-            ) { isGranted ->
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permission ->
+                val isGranted = permission.values.reduce{first,second->(first || second)}
                 if (!isGranted) {
                     Toast.makeText(this, "Brainzplayer requires local storage permissions to play local songs", Toast.LENGTH_SHORT).show()
                 } else {
@@ -47,7 +54,7 @@ class DashboardActivity : ComponentActivity() {
                 }
             }
             SideEffect {
-                launcher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                launcher.launch(neededPermissions)
             }
 
             val backdropScaffoldState =
