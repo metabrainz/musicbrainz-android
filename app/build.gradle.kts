9e19e1249e9a0b38ad2055987ddd36a50061c8f2
Akshat Tiwari <tiwariakshat03@gmail.com>
2024-07-09 23:49:04 +0530

    Update target sdk version and migrate to kts
    - We need to be API 34 and above
    - Migrated to KTS for better dependency handling

---
+++ app/build.gradle.kts
@@ -0,0 +1,184 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+android {
+    namespace = "org.metabrainz.android"
+    compileSdk = 34
+
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                val keystoreProperties = Properties()
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+    }
+
+    defaultConfig {
+        applicationId = "org.metabrainz.android"
+        targetSdk = 34
+        minSdk = 21
+        versionCode = 58
+        versionName = "7.0.3"
+
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+        viewBinding = true
+        buildConfig = true
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = ".debug"
+        }
+        release {
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            }
+            isMinifyEnabled = false
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets {
+        val sharedTestDir = "src/sharedTest/java"
+        val sharedTestResourcesDir = "src/sharedTest/resources"
+
+        getByName("test") {
+            java.srcDirs(sharedTestDir)
+            resources.srcDirs(sharedTestResourcesDir)
+        }
+
+        getByName("androidTest") {
+            java.srcDirs(sharedTestDir)
+            resources.srcDirs(sharedTestResourcesDir)
+        }
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    lint {
+        abortOnError = false
+    }
+}
+
+dependencies {
+    // AndroidX
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.core.splashscreen)
+
+    // Web Service Setup
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging)
+    implementation(libs.paging)
+
+    // Image downloading and Caching library
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+
+    // Fragment Setup For Kotlin
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.coordinatorlayout)
+    implementation(libs.lifecycle.livedata)
+
+    // Tagger & Metadata Setup
+    implementation(libs.string.similarity)
+
+    // Design Setup
+    implementation(libs.material)
+    implementation(libs.lottie)
+    implementation(libs.onboarding)
+    implementation(libs.share.android)
+
+    // Barcode Scan
+    implementation(libs.barcodescanner)
+
+    // Dagger-Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    ksp(libs.hilt.compiler.androidx)
+
+    // Jetpack Compose
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.util)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material3.window)
+    implementation(libs.compose.animation)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.constraintlayout.compose)
+    implementation(libs.navigation.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.lottie.compose)
+
+    // Test Setup
+    testImplementation(libs.junit)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.arch.testing)
+    testImplementation(libs.hamcrest)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.room.testing)
+
+    androidTestImplementation(libs.test.runner)
+    androidTestImplementation(libs.test.junit)
+    androidTestImplementation(libs.arch.testing)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.espresso.intents)
+    androidTestImplementation(libs.compose.ui.test)
+
+    // Room db
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+
+    // Jetpack Compose accompanists
+    implementation(libs.accompanist.systemuicontroller)
+}
